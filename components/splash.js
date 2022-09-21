bfd5b12a4babaa6f1d2260d36f6bac21a0e70014
yunha <wjh0970@gmail.com>
2022-09-22 08:40:40 +0900

    1days_commit

---
+++ components/splash.js
@@ -0,0 +1,11 @@
+import style from "../styles/splash.module.css";
+
+export default function splash(){
+  return(
+    <>
+      <div className={style.wrap}>
+        
+      </div>
+    </>
+  )
+}
---
+++ pages/homePage.js
@@ -0,0 +1,7 @@
+export default function homePage(){
+  return(
+    <>
+    <h1>홈페이지입니다.</h1>
+    </>
+  )
+}
